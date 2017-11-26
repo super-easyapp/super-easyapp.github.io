a31da868b568965323344e2cd67e2ba489de0c32
Michael Thies <michael@glancemirror.com>
2017-11-26 15:35:09 -0600

    Decreased size of header

---
+++ src/components/HeaderTabs.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import logo from '../images/logo.svg';
+
+const HeaderTabs = () => (
+    <div className="Header-Tabs">
+        <ul>
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+            <li>Item 4</li>
+        </ul>
+    </div>
+);
+
+export default HeaderTabs;
