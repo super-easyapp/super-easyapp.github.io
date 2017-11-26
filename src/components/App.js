d8729fb284703c5325c13cf01abc8d8c3af975ba
Michael Thies <michael@glancemirror.com>
2017-11-26 15:59:08 -0600

    Added scss folder structure

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import '../styles/App.css';
 import Header from './Header';
+import HeaderTabs from './HeaderTabs';
+
 
 class App extends Component {
   render() {
