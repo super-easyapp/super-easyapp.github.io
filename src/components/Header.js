eec23c034f8bf894a8dbb0bacf7c313c4b7fc8a2
Michael Thies <michael@glancemirror.com>
2017-11-26 18:50:00 -0600

    Added hover state to tabs and added avatar with wallet value

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import logo from '../images/logo.svg';
+import avatar from '../images/AvatarWithBadge.png';
 
 const Header = () => (
     <div className="App-header">
         <img src={logo} className="App-logo" alt="quix" />
+
+        <div className="Account-Info">
+            <span>$0.00</span>
+            <img src={avatar} className="Avatar" alt="avatar" />
+        </div>
+
     </div>
 );
 
